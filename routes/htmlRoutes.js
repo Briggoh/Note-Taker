e02a4275a64e3eb5634dd53591e19c000719d32a
Briggoh <H.e.briggs99@gmail.com>
2022-10-23 11:04:29 -0700

    adding content to htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,15 @@
 // Requiring path
 const path = require('path');
+
+// This entails the routing 
+module.exports = (app) => {
+    // GET /notes returns the 'notes.html' file
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+    
+    // GET * returns the 'index.html' file.
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    })
+}
